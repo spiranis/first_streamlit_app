2916b23adaf4031c6fbcf2dd283a9376046a932b
spiranis <sampiranis@gmail.com>
2023-06-08 21:06:20 -0600

    Update streamlit_app.py
    import snowflake-connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
-
+import snowflake.connector
 import requests
+
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")

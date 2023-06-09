67c3ee4986a7a45457733747e9c39f4786ae1b16
spiranis <sampiranis@gmail.com>
2023-06-08 21:26:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,7 @@ my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+
+
+add_my_fruit = streamlit.text_input("What fruit would you like to add?", 'jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
